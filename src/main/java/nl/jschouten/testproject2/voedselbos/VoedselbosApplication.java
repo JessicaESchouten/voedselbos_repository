b14d08f62a01a16c083b726062a8c3cdf0a6baf0
Jessica <jessica.schouten@live.nl>
2022-11-23 14:24:16 +0100

    first commit

---
+++ src/main/java/nl/jschouten/testproject2/voedselbos/VoedselbosApplication.java
@@ -0,0 +1,13 @@
+package nl.jschouten.testproject2.voedselbos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoedselbosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoedselbosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/jschouten/testproject2/voedselbos/VoedselbosApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.jschouten.testproject2.voedselbos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoedselbosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
